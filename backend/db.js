a6a510e466507b1db0954a5198cfa3c93f27bf4f
niel-78 <niel25@student.bth.se>
2026-08-22 15:03:07 +0200

    Updateing gitignore

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,18 @@
+import "dotenv/config";
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-    host: "db",
-    user: "root",
-    password: "password",
-    database: "mydb",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
     dateStrings: true
 });
+
+console.log(
+    process.env.DB_HOST,
+    process.env.DB_NAME
+);
 
 export default db;
